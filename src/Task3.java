38936640f8c0a1022c0a6215f0e0bcbc3b22b027
Damir-Sagngaliev <damir.sagngaliev@yandex.ru>
2025-04-09 21:49:54 +0400

    Задание 3 выполнено

---
+++ src/Task3.java
@@ -0,0 +1,8 @@
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Задание 3");
+        float n = 5.4f;
+        float y = Math.round(n); // (* 10) / 10.0 (double)
+        System.out.println(" Ответ - "+ y);
+    }
+}
