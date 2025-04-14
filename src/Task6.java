932b0fd896333697faa5bb653cc71334b90ac418
Damir-Sagngaliev <damir.sagngaliev@yandex.ru>
2025-04-14 20:00:54 +0400

    Задание 6 выполнено

---
+++ src/Task6.java
@@ -0,0 +1,11 @@
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println("Задание 6");
+        final int s = 6;
+        final int t = 12;
+        int a = (s + t) * 2;
+        int b = s * t;
+        System.out.println("Периметр равен " +  a);
+        System.out.println("Площадь равна " + b);
+    }
+}
